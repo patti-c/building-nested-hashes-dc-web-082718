8d7a998b2a484d770fed587bf8261b17014ebfa0
Patricia Carter <patricia4194@gmail.com>
2018-08-09 20:01:44 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,18 +2,17 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       
-      patriarch: {}
-      matriarch: {}
-      hero: {}
+      patriarch: {},
+      matriarch: {},
+      hero: {},
       hero_friends: {}
-      
     }
       
     :capulet => {
       
-      patriarch: {}
-      matriarch: {}
-      hero: {}
+      patriarch: {},
+      matriarch: {},
+      hero: {},
       hero_friends: {}
       
       }
